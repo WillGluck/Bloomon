0059cd53fcff4b791c42a3dc0b9e986fcc39f5b1
Will Glück <bigwillgluck@gmail.com>
2018-06-04 23:47:57 -0300

    Final commit

--- index.js
+++ index.js
@@ -12,18 +12,18 @@ const production = new Model.Production();
 
 var bouquetSpecsFinished = false
 
-// process.stdout.write("Inform Bouquet Specs\n")
+process.stdout.write("Inform Bouquet Specs\n")
 
 rl.on('line', (input) => { // raised every time a new input happens
 
     if (!input.trim()) {
         bouquetSpecsFinished = true;
-        // process.stdout.write("Bouquet Specs finished, inform flowers\n")
+        process.stdout.write("Bouquet Specs finished, inform flowers\n")
         return;
     }
 
     if (storage.isFull()) {
-        // process.stdout.write("Storage is Full\n")
+        process.stdout.write("Storage is Full\n")
         process.exit(1);
     }
 
@@ -33,9 +33,9 @@ rl.on('line', (input) => { // raised every time a new input happens
         // process.stdout.write(JSON.stringify(production.bouquetsSpecifications[0]) + "\n");
         
         var bouquetSpecAvaliable = production.getAvaliableBouquetSpec(storage);
-        // process.stdout.write("Flower added\n")
+        process.stdout.write("Flower added\n")
         if (bouquetSpecAvaliable) {
-            // process.stdout.write("A new bouquet is avaliable\n")
+            process.stdout.write("A new bouquet is avaliable\n")
             // process.stdout.write(bouquetSpecAvaliable.size + "\n")
             process.stdout.write(JSON.stringify(production.createBouquet(bouquetSpecAvaliable, storage)) + "\n\n");
         }        
@@ -45,7 +45,7 @@ rl.on('line', (input) => { // raised every time a new input happens
         // bouquetSpec.size = input[1];
         // process.stdout.write(JSON.stringify(bouquetSpec.size) + "\n");
         production.addBoquetSpecification(bouquetSpec);        
-        // process.stdout.write("Bouquet Specfication Added\n")
+        process.stdout.write("Bouquet Specfication Added\n")
     }
     
 });
